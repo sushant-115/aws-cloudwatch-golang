637bbbca43e89a6dd9b31bf58e8d3a24fc779b70
sushant <sushant@exotel.in>
2018-08-07 10:44:14 +0530

    syntax fixes

--- list_metric.go
+++ list_metric.go
@@ -1,83 +1,92 @@
 package main
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 
-    "fmt"
-   // "os"
-    "strconv"
-    "time"
+	"fmt"
+	// "os"
+	"strconv"
+	"time"
 )
+
 var t *time.Time
-var pId *string
+var pID *string
 var period *int64
 var stat *string
 var unit *string
-var av string = "Average"
-var st string = "Seconds"
-func getParam (index int,list *cloudwatch.Metric) cloudwatch.GetMetricDataInput{
+var av = "Average"
+var st = "Seconds"
+
+func getParam(index int, list *cloudwatch.Metric) cloudwatch.GetMetricDataInput {
 	tim := time.Now()
-        t = &tim
-        id := "m"+strconv.Itoa(index+1)
-        pId = &id
-        stat = &av
-        unit = &st
-        prd := int64(3000)
-        period = &prd
-        param:= cloudwatch.GetMetricDataInput{
-	EndTime: t , /* required */
-  MetricDataQueries: []*cloudwatch.MetricDataQuery  {
-      Id: pId, /* required */
-     MetricStat:*cloudwatch.MetricStat{
-       Metric:&cloudwatch.Metric { /* required */
-          Dimensions:*cloudwatch.Dimension{list.Dimensions},
-          MetricName:list.MetricName,
-          Namespace: list.Namespace,
-        },
-        Period:period, /* required */
-        Stat: stat,
-        Unit :unit,
-      },
-      ReturnData: true,
-    },
-  StartTime:t,
-  MaxDatapoints: 1000,        
-        }
-  return param
+	t = &tim
+	id := "m" + strconv.Itoa(index+1)
+	pID = &id
+	stat = &av
+	unit = &st
+	prd := int64(3000)
+	period = &prd
+	returnData := true
+	maxDataPoints := int64(1000)
+	metricStat := cloudwatch.MetricStat{
+		Metric: &cloudwatch.Metric{ /* required */
+			Dimensions: []*cloudwatch.Dimension{list.Dimensions[0]},
+			MetricName: list.MetricName,
+			Namespace:  list.Namespace,
+		},
+		Period: period, /* required */
+		Stat:   stat,
+		Unit:   unit,
+	}
+	metricQuery := cloudwatch.MetricDataQuery{
+		Id:         pID, /* required */
+		MetricStat: &metricStat,
+		ReturnData: &returnData,
+	}
+	param := cloudwatch.GetMetricDataInput{
+		EndTime: t, /* required */
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
+			&metricQuery,
+		},
+		StartTime:     t,
+		MaxDatapoints: &maxDataPoints,
+	}
+	return param
 }
 
 func main() {
-//    metric := os.Args[1]
-    namespace := "AWS/EBS"
-    dimensions := "VolumeId"
-    
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+	//    metric := os.Args[1]
+	namespace := "AWS/EBS"
+	dimensions := "VolumeId"
 
-     svc := cloudwatch.New(sess)
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-     result, err := svc.ListMetrics(&cloudwatch.ListMetricsInput{
-     //   MetricName: aws.String(metric),
-        Namespace:  aws.String(namespace),
-        Dimensions: []*cloudwatch.DimensionFilter{
-            &cloudwatch.DimensionFilter{
-                Name: aws.String(dimensions),
-            },
-        },
-    })
-    if err != nil {
-        fmt.Println("Error", err)
-        return
-    }
+	svc := cloudwatch.New(sess)
 
-    for i:=0;i<len(result.Metrics);i++ {
-      res ,err := svc.GetMetricsData(getParam(i,result.Metrics[i]))
-      if err!=nil {
-      fmt.Println(i , "err")
-      }
-	fmt.Println(res)
-    }
+	result, err := svc.ListMetrics(&cloudwatch.ListMetricsInput{
+		//   MetricName: aws.String(metric),
+		Namespace: aws.String(namespace),
+		Dimensions: []*cloudwatch.DimensionFilter{
+			&cloudwatch.DimensionFilter{
+				Name: aws.String(dimensions),
+			},
+		},
+	})
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	for i := 0; i < len(result.Metrics); i++ {
+		paramQuery := getParam(i, result.Metrics[i])
+		res, err := svc.GetMetricData(&paramQuery)
+		if err != nil {
+			fmt.Println(i, "err")
+		}
+		fmt.Println(res)
+	}
 }
