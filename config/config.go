eb5a151da532afeaa831bef305c1d2cd89ac9864
Sushant <sushant@exotel.in>
2018-08-08 04:45:41 +0000

    update config.go

--- config/config.go
+++ config/config.go
@@ -1,28 +1,28 @@
 package config
 
 //Namespace of the metric
-const Namespace string = "AWS/EBS"
+const Namespace string = "AWS/ApplicationELB"
 
 //DimensionName for filter the data points
-const DimensionName string = "VolumeId"
+const DimensionName string = "LoadBalancer"
 
 //DimensionValue for filter the data points
 const DimensionValue string = ""
 
 //Stat value
-const Stat string = "Maximum"
+const Stat string = "Average"
 
 //Unit in which the output will come
 const Unit string = "Count"
 
 //StartTime number of days from today
-const StartTime int = 7
+const StartTime int = 1
 
 //EndTime number of days from today (0 for today)
 const EndTime int = 0
 
 //Period in seconds
-const Period int = 3600 * 12
+const Period int = 300
 
 //MaxDataPoints in result
 const MaxDataPoints int = 1000
