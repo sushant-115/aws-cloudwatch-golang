89d9bdf8e55d6bcbb3beae18c65c270aa6dc1326
sushant <sushant@exotel.in>
2018-08-13 12:39:01 +0530

    email structure

--- emailHtml/app.go
+++ emailHtml/app.go
@@ -4,6 +4,11 @@ import "../structs"
 
 var config = Config{}
 
+type ReportCount struct {
+	Name  string
+	Count int
+}
+
 func Configuration() {
 	config.Read()
 }
@@ -14,7 +19,17 @@ func SendMail(report []structs.Report, costReport string) {
 	destination := "sushant@exotel.in"
 	r := NewRequest([]string{destination, "sushant.gupta@mountblue.io"}, subject)
 	temp := make(map[string]interface{})
+	var reportCountArr []ReportCount
+	rc := make(map[string]int)
+	for i := 0; i < len(report); i++ {
+		rc[report[i].ServiceName]++
+	}
+	for k := range rc {
+		r := ReportCount{k, rc[k]}
+		reportCountArr = append(reportCountArr, r)
+	}
 	temp["report"] = report
 	temp["cost"] = costReport
+	temp["reportCount"] = reportCountArr
 	r.Send("emailHtml/templates/template.html", temp)
 }
